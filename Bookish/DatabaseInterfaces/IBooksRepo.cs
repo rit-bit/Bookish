8949b0ce3bd14e1930ebaeb0b9b0c15692e20a0e
Richard Sutch <richard.sutch@softwire.com>
2021-11-01 17:27:38 +0000

    Starting to play with Dapper

---
+++ Bookish/DatabaseInterfaces/IBooksRepo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Bookish.DatabaseInterfaces
+{
+    public interface IBooksRepo
+    {
+        IEnumerable<Book> GetBooks();
+        bool Insert(Book book);
+        bool Update(Book book);
+        bool Delete(Book book);
+    }
+}
