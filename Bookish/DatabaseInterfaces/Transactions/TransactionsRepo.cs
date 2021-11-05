7fe7db088ae0c5cad75f61b287cabf61b4bfb40c
Richard Sutch <richard.sutch@softwire.com>
2021-11-05 09:48:04 +0000

    Implemented checking in stock Updated check_in time default to include the time

--- Bookish/DatabaseInterfaces/Transactions/TransactionsRepo.cs
+++ Bookish/DatabaseInterfaces/Transactions/TransactionsRepo.cs
@@ -7,13 +7,31 @@ namespace Bookish.DatabaseInterfaces.Transactions
     {
         public bool CheckIn(int stock_id)
         {
-            return false;
+            using var db = DatabaseConnection.GetConnection();
+            var checkedIn = DateTime.Now;
+            var transaction = db.BeginTransaction();
+            var rowsAffected = 0;
+            try
+            {
+                rowsAffected = new NpgsqlCommand(
+                    $"UPDATE transactions SET checked_in = '{checkedIn}' " +
+                    $"WHERE stock_id = {stock_id} AND checked_in IS NULL",
+                    db, transaction).ExecuteNonQuery();
+
+                transaction.Commit();
+            }
+            catch (NpgsqlException e)
+            {
+                Console.WriteLine(e);
+            }
+
+            return rowsAffected == 1;
         }
 
         public bool CheckOut(int stock_id, int user_id, DateTime? due_back)
         {
             using var db = DatabaseConnection.GetConnection();
-            due_back ??= DateTime.Today.AddDays(14);
+            due_back ??= DateTime.Now.AddDays(14);
             var transaction = db.BeginTransaction();
             var rowsAffected = 0;
             try
